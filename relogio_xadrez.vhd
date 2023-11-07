0c309b3d75db69a0b7d91c9a5fa3550746844b2d
kamipborges <131539243+kamipborges@users.noreply.github.com>
2023-11-07 18:32:26 -0300

    Update relogio_xadrez.vhd

--- relogio_xadrez.vhd
+++ relogio_xadrez.vhd
@@ -23,57 +23,95 @@ end relogio_xadrez;
 
 architecture relogio_xadrez of relogio_xadrez is
     -- DECLARACAO DOS ESTADOS
-    type states is ( reset, load, start, j1, j2, win_j1, win_j2);
+    type states is ( init, carregar, start, jog1, jog2, win_j1, win_j2);
     signal EA, PE : states;
     -- ADICIONE AQUI OS SINAIS INTERNOS NECESSARIOS
     -- sinais internos
-    signal en_1, en_2
+    signal en_1, en_2, load_int;   --sinais internos
+   
 begin
 
     -- INSTANCIACAO DOS CONTADORES
     contador1 : entity work.temporizador port map (         
-            clock => clock_int,
-            reset => reset_int,
-            load => load_int,
+            --clock relogio_xadrez => clock do temporizador
+            clock => clock,                             --in
+            reset => reset,      
+            load_int => load,
             en => en_1,
-            init_time => init_time_int,
-            cont => contj1_int
+            init_time => init_time,
+            cont => contj1   --saida eh o contrario 
         );
 
     contador2 : entity work.temporizador port map (
-        clock => clock_int,
-        reset => reset_int,
-        load => load_int,
-        en => en_2,
-        init_time => init_time_int,
-        cont => contj2_int
-    );
+        contador1 : entity work.temporizador port map (         
+            --clock relogio_xadrez => clock do temporizador
+            clock => clock,    
+            reset => reset,
+            load_int => load,
+            en => en_1,
+            init_time => init_time,
+            cont => contj2   --saida eh o contrario 
+        );
 
 
     -- PROCESSO DE TROCA DE ESTADOS
     process (clock, reset)
-    begin
-        
-        -- COMPLETAR COM O PROCESSO DE TROCA DE ESTADO
-
+        if reset = '1' then 
+            EA <= init
+        elsif rising_edge(clock) then
+            EA <= PE
+        end if;
     end process;
 
     -- PROCESSO PARA DEFINIR O PROXIMO ESTADO
     process ( ) --<<< Nao esqueca de adicionar os sinais da lista de sensitividade
     begin
         case EA is
-            
-            --COMPLETAR O CASE PARA CADA UM DOS ESTADOS DA SUA MAQUINA
+            when init => if carregar = '1' then
+                        PE <= carregar;
+                    else 
+                        PE <= init;
+                    end if;
+                    when carregar => 
+                    PE <= start;
+               
+                    when start => 
+                    if j1 = '1' then 
+                    PE <= j1
+                    elseif j2='1'
+                    PE <= j2;
+                    end if;
+               
+                    when j1 =>
+                    if j1 ='1' then 
+                    PE <= j2;
+                    end if;
+                    if count1 = "0000" then 
+                    PE <= win_j2;
+                    end if;
+               
+                    when j2 =>
+                    if j2 ='1' then 
+                    PE <= j1;
+                    end if;
+                    if count2 = "0000" then
+                    PE <= win_j1;
+                    end if;
 
         end case;
     end process;
 
     
     -- ATRIBUICAO COMBINACIONAL DOS SINAIS INTERNOS E SAIDAS - Dica: faca uma maquina de Moore, desta forma os sinais dependem apenas do estado atual!!
-    en_1 <= '1' when EA = j1 else '0';
+    en_1 <= '1' when EA = j1 else '0'; --decrementar 
+    en_2 <= '1' when EA = j2 else '0';
+    load_int <= '1' when EA = load else '0'; --
+    j1 <= '1' then 
     
 
+
 end relogio_xadrez;
+--
 
 
 
